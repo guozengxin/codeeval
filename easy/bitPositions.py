39c4de48e43830808aa02b081caf2d0993a88596
guozengxin <guozengxin@outlook.com>
2014-04-19 22:58:58 +0800

    add codeeval init files and .gitignore

---
+++ easy/bitPositions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# encoding=utf8
+
+import sys
+
+
+def process(line):
+    arr = line.split(',')
+    n, p1, p2 = int(arr[0]), int(arr[1]), int(arr[2])
+    num1 = 1 << (p1 - 1)
+    num2 = 1 << (p2 - 1)
+    if ((n & num1) >> (p1 - 1)) == ((n & num2) >> (p2 - 1)):
+        print 'true'
+    else:
+        print 'false'
+
+
+def main(argv):
+    if len(argv) < 1:
+        sys.exit(1)
+    inputFile = argv[0]
+    fp = open(inputFile, 'r')
+    for line in fp:
+        process(line.strip())
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
---
+++ moderate/longestLines.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# encoding=utf8
+
+import sys
+
+import heapq
+
+
+class MaxHeap(object):
+    def __init__(self, x):
+        self.heap = [e for e in x]
+        heapq.heapify(self.heap)
+
+    def push(self, value):
+        heapq.heappush(self.heap, value)
+
+    def pop(self):
+        return heapq.heappop(self.heap)
+
+
+def main(argv):
+    inputFile = argv[0]
+    fp = open(inputFile)
+    N = None
+    maxHeap = None
+    for line in fp:
+        line = line.strip()
+        if N is None:
+            N = int(line)
+            maxHeap = MaxHeap([])
+        else:
+            maxHeap.push([-(len(line)), line])
+    for i in range(N):
+        print maxHeap.pop()[1]
+    print
+    fp.close()
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
